8301e298a61d363eab10b7adad8316d7b84dc6f5
gaoxugang1 <1253652573@qq.com>
2018-07-15 15:18:36 +0800

    Initial commit

---
+++ src/main/java/com/xyz/java/base/regex/RegexUtils.java
@@ -0,0 +1,28 @@
+package com.xyz.java.base.regex;
+
+import java.util.regex.Pattern;
+
+/**
+ * 正则表达式工具类
+ * Created by Lenovo on 2018/7/15.
+ */
+public class RegexUtils {
+    /**
+     * 工具包：java.util.regex
+     * 使用到的类：
+     *              Pattern
+     *              Matcher
+     *              PatternSyntaxException
+     */
+
+
+    public static boolean isMatch(String regex,String matchStr){
+        return Pattern.matches(regex,matchStr);
+    }
+
+    public static void main(String[] args) {
+        String context = "abcefg";
+        String regex = "^abc";
+        System.out.println(isMatch(regex,context));
+    }
+}
