433db9e2656a6a49e472b68738a304ec427ab374
gaoxugang1 <1253652573@qq.com>
2018-08-05 22:08:37 +0800

    PM:集合类测试

--- src/main/java/com/xyz/java/base/collection/ArrayListTest.java
+++ src/main/java/com/xyz/java/base/collection/ArrayListTest.java
@@ -8,11 +8,22 @@ import java.util.List;
  * Created by Lenovo on 2018/7/29.
  */
 public class ArrayListTest {
+    /**
+     * 提交测试
+     * @param args
+     */
     public static void main(String[] args) {
         List arrayList = new ArrayList();
         arrayList.add("string1");
         arrayList.add(new Date());
         System.out.println(arrayList.toString());
 
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i+"w");
+        }
+        System.out.println(list.size());
+        list.clear();
+        System.out.println(list.size());
     }
 }
