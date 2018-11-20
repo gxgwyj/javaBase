69e21ba0c60698684dbc4e2ba4c3fc198d2d2a78
gxgwyj <498868779@qq.com>
2018-11-20 17:48:20 +0800

    添加DES加密方式

---
+++ src/main/java/com/xyz/java/base/enums/TestEnum.java
@@ -0,0 +1,14 @@
+package com.xyz.java.base.enums;
+
+/**
+ * 类: TestEnum <br>
+ * 描述: 枚举测试<br>
+ * 作者:  gaoxugang<br>
+ * 时间: 2018年11月20日 11:50
+ */
+public class TestEnum {
+    public static void main(String[] args) {
+        System.out.println(WeekEnum.MON.ordinal());
+        System.out.println(WeekEnum.MON);
+    }
+}
---
+++ src/main/java/com/xyz/java/base/enums/WeekEnum.java
@@ -0,0 +1,18 @@
+package com.xyz.java.base.enums;
+
+/**
+ * 类: WeekEnum <br>
+ * 描述: <br>
+ * 作者:  gaoxugang<br>
+ * 时间: 2018年11月20日 11:38
+ */
+public enum WeekEnum {
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    SAT,
+    SUN,
+    ;
+}
