2e5ff135386df7eaa4aaceb0f7c0229b37382c99
gaoxugang1 <1253652573@qq.com>
2019-01-23 22:45:19 +0800

    PM:重构方法

---
+++ src/main/java/com/xyz/java/base/LoopTest.java
@@ -0,0 +1,25 @@
+package com.xyz.java.base;
+
+/**
+ * Created by Lenovo on 2019/1/23.
+ */
+public class LoopTest {
+    public static void main(String[] args) {
+        int i = 100;
+        if (i++ > 100) {
+            System.out.println("i++");
+        }
+        if (++i > 100) {
+            System.out.println("++i");
+        }
+
+        int b = 5;
+        int d = 5;
+        int c = 100+(b++);
+        int e = 100+(++d);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(b);
+        System.out.println(d);
+    }
+}
