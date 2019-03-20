e0ee7696b6942bfc4f49ea9af2cdff92d84c2148
gxgwyj <498868779@qq.com>
2019-03-20 10:47:34 +0800

    String变量操作

---
+++ src/main/java/com/xyz/java/base/String/StringOperate.java
@@ -0,0 +1,38 @@
+package com.xyz.java.base.String;
+
+import org.junit.Test;
+
+/**
+ * 类: StringOperate <br>
+ * 描述: 字符串操作<br>
+ * 作者:  gaoxugang<br>
+ * 时间: 2019年03月20日 10:35
+ */
+public class StringOperate {
+
+    @Test
+    public void operateStringBuilder() {
+        StringBuilder stringBuilder = new StringBuilder();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 9000000; i++) {
+            stringBuilder.append("abc");
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("StringBuilder 使用时间：" + (end - start));
+    }
+
+    @Test
+    public void operateStringBuffer() {
+        StringBuffer stringBuffer = new StringBuffer();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 9000000; i++) {
+            // StringBuffer中的append方法是同步方法，会有性能影响
+            stringBuffer.append("abc");
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("StringBuffer 使用时间：" + (end - start));
+    }
+
+}
--- src/main/java/com/xyz/java/base/operate/JOperator.java
+++ src/main/java/com/xyz/java/base/operate/JOperator.java
@@ -10,30 +10,30 @@ import org.junit.Test;
  */
 public class JOperator {
 
-    public static void main(String[] args) {
-    }
-
     /**
     * 位移计算公式  向左移动一位相当于基数乘以2
      * 左移：n << m   n乘以2的m次方
      * 右移：n >> m
     */
-    public static int leftMove(int i ,int size) {
+    @Test
+    public void leftMove() {
+        int  i = 585;
+        int size = 3;
         System.out.println(String.format("将%d向左位移%d位",i,size));
         System.out.println("移动前:" + Integer.toBinaryString(i));
         int result = i << size;
         System.out.println("移动后:" + Integer.toBinaryString(result));
-        return result;
     }
 
     /**
      * ^ 亦或运算  比较的两个整数，二进制位运算，相同的为0，不同的为1
-     * @param a
-     * @param b
      * @return
      */
-    public static int orOpt(int a, int b) {
-        return a ^ b;
+    @Test
+    public void orOpt() {
+        int a = 809;
+        int b = 809;
+        System.out.println(a ^ b);
     }
 
     /**
@@ -44,13 +44,16 @@ public class JOperator {
      *  00000000000
      * @param b
      */
-    public static int andOpt(int a, int b) {
-        return a & b;
+    @Test
+    public void andOpt() {
+        int a = 10;
+        int b = 11;
+        System.out.println(a & b);
     }
 
     /**
      * 无符号的右移
-     * >>>:无符号右移，忽略符号位，空位都以0补齐
+     * >>>:无符号右移，忽略符号位，空位都以0补齐(高位补零、低位舍弃)
      * 1111111 11111111 11111111 11111111
      *
      */
@@ -62,8 +65,27 @@ public class JOperator {
     }
 
     /**
-     * 首先，假设有一种情况，
-     * 对象 A.hashCode = 1000010001110001000001111000000
-     * 对象 B.hashCode = 0111011100111000101000010100000
+     * 对于一个有多个数值的数组，只有一个是唯一的，其他都是成对的，怎样快速找到这个唯一值
+     * 根据亦或预算符号出的一道编程题(该题就是利用java亦或运算符的操作特性)
+     * 关于上边的这个结果是什么，你只需要知道异或运算的特点就可以了，
+     * 比如1,2,3,2,3 ,既然异或运算满足上边的交换规则，对于1^2^3^2^3 这样的异或运算，
+     * 我们换一下位置(2^2)^(3^3)^1 ，通过两个相同的数进行异或运算得到0 ,那最终就会是0^0^1 ,最后会得到1 。
+     *
+     */
+    @Test
+    public void findUniqueData() {
+        int[] numarry = new int[]{8, 2, 3, 2, 3};
+        int aim = numarry[0];
+        for(int i = 1; i < 5; i++)
+        {
+            aim = aim ^ numarry[i];
+        }
+        System.out.println("最后："+aim);
+    }
+
+
+    /**
+     * 总结
+     * &：与运算 操作数中的位都为1，结果才为1
      */
 }
