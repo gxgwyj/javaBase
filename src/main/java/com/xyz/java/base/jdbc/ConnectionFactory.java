fc08b302db2130ecf1cbcee1a5b1d91e5524ea11
gxg <498868779@qq.com>
2019-03-25 22:21:53 +0800

    添加jdbc

---
+++ src/main/java/com/xyz/java/base/jdbc/ConnectionFactory.java
@@ -0,0 +1,28 @@
+package com.xyz.java.base.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+    private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/workplatform?characterEncoding=utf8";
+    private static final String USER_NAME = "root";
+    private static final String PASS_WORD = "root";
+
+    public static Connection getConnection()  {
+        try {
+            Class.forName(DRIVER_CLASS);
+            Connection connection = DriverManager.getConnection(URL,USER_NAME,PASS_WORD);
+            return connection;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getConnection());
+    }
+
+
+}
