b52d9924deb6effa7926796f379dd029562d7607
高旭刚 <gaoxugang@ucfgroup.com>
2019-07-02 18:42:04 +0800

    指令重排序

--- src/main/java/com/xyz/java/base/multithread/VolatileAction.java
+++ src/main/java/com/xyz/java/base/multithread/VolatileAction.java
@@ -47,6 +47,36 @@ public class VolatileAction {
                 }
             }
         },"B").start();
+
+
+        codeReordering();
+
+    }
+
+    /**
+     * 指令重排序解释
+     */
+    public static void codeReordering() {
+        int x, y;
+        x = 1;
+        try {
+            /**
+             * y = 0 / 0可能会被重排序在x = 2之前执行，为了保证最终不致于输出x = 1的错误结果，
+             * JIT在重排序时会在catch语句中插入错误补偿代码，将x赋值为2，将程序恢复到发生异常时应有的状态。
+             * 这种做法的确将异常捕捉的逻辑变得复杂了，但是JIT的优化的原则是，尽力优化正常运行下的代码逻辑，
+             * 哪怕以catch块逻辑变得复杂为代价，毕竟，进入catch块内是一种“异常”情况的表现。
+             */
+            //
+            x = 2;
+            y = 0 / 0;
+        } catch (Exception e) {
+            /**
+             * 此处JIT在重排序中插入错误补偿代码将x赋值为2
+             */
+        } finally {
+            System.out.println("x = " + x);
+        }
+
     }
 
 
