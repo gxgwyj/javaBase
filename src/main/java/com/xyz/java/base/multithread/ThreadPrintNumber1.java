9c40c9eb731e5e97ac19e1c9131c53b14458de00
高旭刚 <gaoxugang@ucfgroup.com>
2019-07-29 15:45:20 +0800

    多线程交替打印奇偶数

---
+++ src/main/java/com/xyz/java/base/multithread/ThreadPrintNumber1.java
@@ -0,0 +1,58 @@
+package com.xyz.java.base.multithread;
+
+/**
+ * @author Administrator
+ * @data 2019/7/29
+ * @description 面试题，多线程交替打印奇偶数
+ */
+public class ThreadPrintNumber1 {
+
+    public static void main(String[] args) {
+
+        Object object = new Object();
+
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (object) {
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println(Thread.currentThread().getName() + "=" + (2 * i + 1));
+                        object.notify();
+                        try {
+                            object.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (object) {
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println(Thread.currentThread().getName() + "=" + (2 * i + 2));
+                        // 必须先通知，再等待
+                        object.notify();
+                        try {
+                            object.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            }
+        });
+
+
+        t1.start();
+        t2.start();
+
+    }
+
+}
---
+++ src/main/java/com/xyz/java/base/multithread/ThreadPrintNumber2.java
@@ -0,0 +1,60 @@
+package com.xyz.java.base.multithread;
+
+/**
+ * @author Administrator
+ * @data 2019/7/29
+ * @description 使用volatile关键字进行打印
+ */
+public class ThreadPrintNumber2 {
+
+    /**
+     * volatitle 保证在多线程的环境中，各个线程对内存的可见性
+     */
+    static volatile int num = 0;
+    static volatile boolean flag = false;
+
+    public static void main(String[] args) {
+
+        // 线程一
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (; num < 100; ) {
+                    if (!flag && (num == 0 || ++num % 2 == 0)) {
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(Thread.currentThread().getName() + "[偶数]=" + num);
+                        flag = true;
+                    }
+                }
+            }
+        });
+        // 线程2
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (; num < 100; ) {
+                    if (flag && (++num % 2 != 0)) {
+
+                        try {
+                            // 防止打印速度过快导致混乱
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            //NO
+                        }
+                        System.out.println(Thread.currentThread().getName() + "[奇数]=" + num);
+                        flag = false;
+                    }
+                }
+            }
+        });
+
+
+        t1.start();
+        t2.start();
+    }
+
+}
