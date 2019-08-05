be4e0b3793305e26d5d8fbd420d4c76664ac797d
gxg <498868779@qq.com>
2019-08-05 23:01:21 +0800

    添加多线程实例1

--- src/main/java/com/xyz/java/base/collection/TreeTraverse.java
+++ src/main/java/com/xyz/java/base/collection/TreeTraverse.java
@@ -62,5 +62,22 @@ public class TreeTraverse {
         System.out.println(ele.data);
     }
 
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    public static void main(String[] args) {
+        int sizeFor = tableSizeFor(12);
+        System.out.println(sizeFor);
+    }
+
 
 }
