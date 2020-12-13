f6d2a0efe3ad8605b67eebc45f85dfd0393dbac0
gxg <498868779@qq.com>
2020-12-13 13:36:26 +0800

    添加多线程实例

---
+++ src/main/java/com/xyz/java/base/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.xyz.java.base;
+
+/**
+ * @author gaoxugang
+ * @data 2020/12/12 0012 20:53
+ * @description TODO
+ */
+public class HelloWorld  {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
