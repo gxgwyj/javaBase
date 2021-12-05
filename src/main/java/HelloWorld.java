4a4b69eaad775b194cb816334c2a97ed0b7de453
gxg <498868779@qq.com>
2021-12-05 17:10:57 +0800

    添加多线程实例

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author gaoxugang
+ * @data 2021/12/4  21:00
+ * @description first java program
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
