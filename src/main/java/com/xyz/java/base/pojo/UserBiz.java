f9544c06d4c22de89e6895f56d6c1802e61ecbe1
gxg <498868779@qq.com>
2022-03-16 19:12:02 +0800

    添加多线程实例

---
+++ src/main/java/com/xyz/java/base/pojo/UserBiz.java
@@ -0,0 +1,18 @@
+package com.xyz.java.base.pojo;
+
+/**
+ * @author gaoxugang
+ * @data 2022/3/12 0012 19:02
+ * @description
+ */
+public class UserBiz {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.eat();
+    }
+}
