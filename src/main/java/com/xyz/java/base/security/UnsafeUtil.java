f2c27698531784a6919d684a5840acc14783738a
gxg <498868779@qq.com>
2022-05-04 14:39:45 +0800

    添加多线程实例

--- src/main/java/com/xyz/java/base/security/UnsafeUtil.java
+++
@@ -1,7 +0,0 @@
-package com.xyz.java.base.security;
-
-public class UnsafeUtil {
-    public static void main(String[] args) {
-        sun.misc.Unsafe UNSAFE = sun.misc.Unsafe.getUnsafe();
-    }
-}
